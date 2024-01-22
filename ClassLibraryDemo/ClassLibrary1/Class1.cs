7ce7c3fc43f2cad3aed223a420c6a943b285342a
Chaitanya5963 <156076665+Chaitanya5963@users.noreply.github.com>
2024-01-22 06:57:45 -0800

    Add files via upload

---
+++ ClassLibraryDemo/ClassLibrary1/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        int a = 5;
+        public void print()
+        {
+            Console.WriteLine("The value of a is " + a);
+        }
+    }
+}

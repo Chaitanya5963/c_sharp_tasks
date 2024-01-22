cd36e1e8e49f90fecc64fd3e77bf6cdec43b1f27
Chaitanya5963 <156076665+Chaitanya5963@users.noreply.github.com>
2024-01-22 07:26:12 -0800

    Add files via upload

---
+++ ITGTraining24/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITGTraining24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyFirstClass obj = new MyFirstClass();
+            obj.s = Console.ReadLine();
+            Console.WriteLine("The result is "+obj.s);
+            Console.ReadLine();
+        }
+    }
+}
